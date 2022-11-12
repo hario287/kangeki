ebd9fed7037e80d4a9390187e6a9c411c418ea81
EC2 Default User <mhyrso27@gmail.com>
2022-11-12 01:46:27 +0000

    [add]admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+root 'public/homes#top'
+
 end
