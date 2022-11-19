2833a1c37df0c761e27431b9d0cf96401c2d1dee
EC2 Default User <mhyrso27@gmail.com>
2022-11-19 07:44:17 +0000

    [fix]comments-error

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,10 +1,27 @@
 class Public::PostCommentsController < ApplicationController
-  def create
-  end
 
-  def index
+  def create
+    @post = Post.find(params[:post_id])
+    post_comment = current_user.post_comments.new(post_comment_params)
+    post_comment.post_id = @post.id
+    if post_comment.save
+      flash.now[:notice] = "コメントしました"
+      redirect_to post_path(@post)
+    else
+      render :error
+    end
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
+    post_comment = PostComment.find(params[:id])
+    post_comment.destroy
+    flash.now[:alert] = "コメントを削除しました"
+    redirect_to post_path(@post)
   end
+
+  private
+    def post_comment_params
+      params.require(:post_comment).permit(:comment, :post_id)
+    end
 end
--- app/controllers/public/review_comments_controller.rb
+++ app/controllers/public/review_comments_controller.rb
@@ -1,10 +1,25 @@
 class Public::ReviewCommentsController < ApplicationController
   def create
-  end
-
-  def index
+    @review = Review.find(params[:review_id])
+    @review_comment = current_user.review_comments.new(review_comment_params)
+    @review_comment.review_id = @review.id
+    if @review_comment.save
+      flash.now[:notice] = "コメントしました"
+      render :create
+    else
+      render :error
+    end
   end
 
   def destroy
+    @review = Review.find(params[:review_id])
+    ReviewComment.find(params[:id]).destroy
+    flash.now[:alert] = "コメントを削除しました"
+    render :destroy
   end
+
+  private
+    def review_comment_params
+      params.require(:review_comment).permit(:comment)
+    end
 end
