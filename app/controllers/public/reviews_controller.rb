42e690ffc1427964230d5f0eee4974e3cc7cf1a0
EC2 Default User <mhyrso27@gmail.com>
2022-11-19 12:57:07 +0000

    [fix]review-error

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -8,43 +8,35 @@ class Public::ReviewsController < ApplicationController
   def show
     @review = Review.find(params[:id])
     @review_comment = ReviewComment.new
-
+    @review_tags = @review.tags
   end
 
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
-    tag_list = params[:review][:tag_name].split(',')
+    # 受け取った値を,で区切って配列にする
+    tag_list = params[:review][:name].split(',')
     if @review.save
       @review.save_tag(tag_list)
-      redirect_to review_path(@review.id), notice: "投稿しました。"
+      redirect_to review_path(@review), notice: "投稿しました。"
     else
-      @reviews = Review.all
       render 'index'
     end
   end
 
   def index
-    if params[:sort] == nil
-      to = Time.current.at_end_of_day
-      from = to - 1.week
-      @reviews = Review.includes(:favorites).sort {
-        |a,b| b.favorites.where(created_at: from..to).size <=>
-        a.favorites.where(created_at: from..to).size
-        }
-    else
-      @reviews = Review.all.order(params[:sort])
-    end
-       @review = Review.new
+    @reviews = Review.page(params[:page]).per(10)
+    @tag_list = Tag.all
   end
 
   def edit
     @review = Review.find(params[:id])
+    @tag_list = @review.tags.pluck(:name).join(',')
   end
 
   def update
     @review = Review.find(params[:id])
-    tag_list = params[:review][:tag_name].split(',')
+    tag_list = params[:review][:name].split(',')
     if @review.update(review_params)
       @review.save_tag(tag_list)
       redirect_to review_path(@review.id), notice: "投稿を編集しました。"
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,9 +1,9 @@
 class Tag < ApplicationRecord
   has_many :review_tags,dependent: :destroy, foreign_key: 'tag_id'
-  # タグは複数の投稿を持つ　それは、review_tag_relationshipsを通じて参照できる
+  # タグは複数の投稿を持つ　それは、review_tagsを通じて参照できる
   has_many :reviews,through: :review_tags
 
-  scope :merge_books, -> (tags){ }
+  scope :merge_reviews, -> (tags){ }
 
   def self.search_reviews_for(content, method)
 
